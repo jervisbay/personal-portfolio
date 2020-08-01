1c80964b520244523a69e49c951905f188c51c8e
Jervis Bay <jervis.bay@gmail.com>
2020-08-01 08:49:47 -0400

    simplified page

--- client/src/components/AboutMe/index.js
+++ client/src/components/AboutMe/index.js
@@ -1,50 +1,29 @@
 import React from "react";
 import "../../style.css";
 
-
 function AboutMe() {
-    return (
-        <section id="about-me">
-        <h1 class="section-title">About Me</h1>
-        <br />
-        <br />
-        <div class="container flex-about-container">
-            <div class="flex-about-text-1">
-                <img src={require("./Flag_of_Singapore.svg")} id="flex-image"></img>
-            </div>
-            <div class="flex-about-text-2" id="gray-background">
-                <p class="about-text">Born in <span id="red">Singa</span><span id="white">pore</span>.<br /> Studied in Virginia.
-                </p>
-            </div>
+  return (
+    <section id="about-me">
+      <h1 class="section-title">About Me</h1>
+      <br />
+      <br />
+      <div class="container flex-about-container ">
+        <div class="about-text-container">
+          I was born in Singapore and came over to the US to attned college at
+          the University of Virginia.
+          <br /><br />
+          Upon graduating, I have worked in both New York and San Francisco, all
+          within Finance.
+          <br /><br />
+          Being an 'Excel' geek, this website is a product of a coding bootcamp
+          I embarked on.
+          <br /><br />
+          I currently live in Brooklyn, NY, with my wife and silly Corgi.
+
         </div>
-    
-        <br /><br />
-    
-        <div class="container flex-about-container">
-            <div class="flex-about-text-1">
-                <img src={require("./ny-skyline.jpg")} id="flex-image"></img>
-    
-            </div>
-            <div class="flex-about-text-2" id="gray-background">
-                <p class="about-text">
-                    Worked in: <br /> New York <br /> San Francisco
-                </p>
-            </div>
-        </div>
-    
-        <br /><br />
-    
-        <div class="container flex-about-container">
-            <div class="flex-about-text-1">
-                <img src={require("./tracy-waffles.jpeg")} id="flex-image"></img>
-            </div>
-            <div class="flex-about-text-2" id="gray-background">
-                <p class="about-text">Currently living in Brooklyn with my wife and corgi.</p>
-            </div>
-        </div>
-    
+      </div>
     </section>
-    );
-  }
-  
-  export default AboutMe;
+  );
+}
+
+export default AboutMe;
